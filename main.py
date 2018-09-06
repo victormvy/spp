463688e502576c31d3f059d37bef9e25339bf850
tr1tu <victormvy@gmail.com>
2018-09-06 12:08:25 +0200

    Train repetitions

--- main.py
+++ main.py
@@ -25,10 +25,11 @@ def cli():
 @click.option('--spp_alpha', default=0.2, help=u'Alpha value for spp transfer function')
 @click.option('--lr', default=0.1, help=u'Learning rate')
 @click.option('--momentum', '-m', default=0.1, help=u'Momentum for optimizer')
-def main(db, net_type, batch_size, epochs, checkpoint_dir, log_dir, activation, spp_alpha, lr, momentum):
-	train(db, net_type, batch_size, epochs, checkpoint_dir, log_dir, activation, spp_alpha, lr, momentum)
+@click.option('--rep', '-r', default=1, help=u'Repetitions for this execution.')
+def main(db, net_type, batch_size, epochs, checkpoint_dir, log_dir, activation, spp_alpha, lr, momentum, rep):
+	train(db, net_type, batch_size, epochs, checkpoint_dir, log_dir, activation, spp_alpha, lr, momentum, rep)
 
-def train(db, net_type, batch_size, epochs, checkpoint_dir, log_dir, activation, spp_alpha, lr, momentum):
+def train(db, net_type, batch_size, epochs, checkpoint_dir, log_dir, activation, spp_alpha, lr, momentum, rep):
 	train = None
 	test = None	
 	num_channels = 3	
@@ -101,98 +102,113 @@ def train(db, net_type, batch_size, epochs, checkpoint_dir, log_dir, activation,
 	optimizer = tf.train.MomentumOptimizer(learning_rate=lrt, momentum=momentum, use_nesterov=True).minimize(cost)
 
 	saver = tf.train.Saver()
+	
+	if not os.path.exists(checkpoint_dir):
+		os.makedirs(checkpoint_dir)
+	if not os.path.exists(log_dir):
+		os.makedirs(log_dir)	
+		
+	start_epoch = 1
+	best_test_acc = 0.0
+	start_execution = 1
+	
+	# Read file of current epoch
+	if os.path.isfile(os.path.join(checkpoint_dir, 'current_epoch.txt')):
+		with open(os.path.join(checkpoint_dir, 'current_epoch.txt'), 'r') as f:
+			start_epoch = int(f.readline())
+			best_test_acc = float(f.readline())
+			start_execution = int(f.readline())
+	
+	results = {'best_test_acc': []}
 
-	with tf.Session() as sess:
-		if not os.path.exists(checkpoint_dir):
-			os.makedirs(checkpoint_dir)
-		if not os.path.exists(log_dir):
-			os.makedirs(log_dir)
+	for execution in range(start_execution, rep + 1):
+		execution_checkpoint = '{}/{}'.format(checkpoint_dir, execution)
+		execution_log = '{}/{}'.format(log_dir, execution)
+		with tf.Session() as sess:
+			
+			sess.run(tf.global_variables_initializer())
+	
+			if os.path.isfile(os.path.join(checkpoint_dir, "checkpoint")):
+				try:
+					saver.restore(sess, os.path.join(execution_checkpoint, checkpoint_file))
+					print("Restored checkpoint")
+				except tf.errors.NotFoundError:
+					print("No restore point found")
 
-		sess.run(tf.global_variables_initializer())
-
-		if os.path.isfile(os.path.join(checkpoint_dir, "checkpoint")):
-			try:
-				saver.restore(sess, os.path.join(checkpoint_dir, checkpoint_file))
-				print("Restored checkpoint")
-			except tf.errors.NotFoundError:
-				print("No restore point found")
-
-		start_epoch = 1
+			
+			writer = tf.summary.FileWriter(execution_log, sess.graph)
+			
+			curr_lr = lr
+			
+			for epoch in range(start_epoch, epochs + 1):		
+				tStart = time.time()
+				
+				# Reduce learning rate in epochs 60, 80 and 90.
+				if epoch in { 60, 80, 90 }:
+					if curr_lr > 0.02:
+						curr_lr -= 0.02
+						print('Learning rate reduced ({})'.format(curr_lr))
+					else:
+						print('Learning rate could not be reduced ({})'.format(curr_lr))
+				
+				current = 0
+				mean_train_acc = 0
+				num_batches = 0
+				while current < len(train_x):
+					_, acc = sess.run([optimizer, accuracy], feed_dict={
+						x: train_x[current:current + batch_size],
+						y_true: train_y[current:current + batch_size],
+						lrt: curr_lr
+					})
+	
+					print("Epoch {}/{}. Images from {} to {} of {}. Accuracy: {}".format(epoch, epochs, current,
+																				   current + batch_size, len(train_x), acc))
+					current = current + batch_size
+					mean_train_acc += acc
+					num_batches += 1
+	
+				mean_train_acc /= num_batches
+	
+				# Test accuracy
+				mean_test_acc = 0
+				current = 0
+				num_batches = 0
+				while current < len(test_x):
+					num_batches += 1
+					test_acc = sess.run(accuracy, feed_dict={
+						x: test_x[current:current + batch_size],
+						y_true: test_y[current:current + batch_size],
+						lrt: curr_lr
+					})
+					mean_test_acc += test_acc
+					current = current + batch_size
+	
+				mean_test_acc /= num_batches
+	
+				if mean_test_acc > best_test_acc:
+					best_test_acc = mean_test_acc
+	
+				tElapsed = time.time() - tStart
+	
+				print("End of epoch {}. Train accuracy: {}, Test accuracy: {}, Time: {}s".format(epoch, mean_train_acc,
+																								 mean_test_acc, tElapsed))
+	
+				summary = tf.Summary()
+				summary.value.add(tag="Train accuracy", simple_value=mean_train_acc)
+				summary.value.add(tag="Test accuracy", simple_value=mean_test_acc)
+				writer.add_summary(summary, epoch)
+				print("Summary saved!")
+	
+				# Save current epoch and execution in file
+				with open(os.path.join(checkpoint_dir, 'current_epoch.txt'), 'w') as f:
+					f.write("{}\n{}\n{}".format(str(epoch + 1), str(best_test_acc), str(execution + 1)))
+	
+				# Save result
+				results['best_test_acc'].append(best_test_acc)
+		start_epoch = 0
 		best_test_acc = 0.0
-
-		# Read file of current epoch
-		if os.path.isfile(os.path.join(checkpoint_dir, 'current_epoch.txt')):
-			with open(os.path.join(checkpoint_dir, 'current_epoch.txt'), 'r') as f:
-				start_epoch = int(f.readline())
-				best_test_acc = float(f.readline())
-
-		writer = tf.summary.FileWriter(log_dir, sess.graph)
-
-		for epoch in range(start_epoch, epochs + 1):		
-			tStart = time.time()
-			
-			# Reduce learning rate in epochs 60, 80 and 90.
-			if epoch in { 60, 80, 90 }:
-				if lr > 0.02:
-					lr -= 0.02
-					print('Learning rate reduced ({})'.format(lr))
-				else:
-					print('Learning rate could not be reduced ({})'.format(lr))
-			
-			current = 0
-			mean_train_acc = 0
-			num_batches = 0
-			while current < len(train_x):
-				_, acc = sess.run([optimizer, accuracy], feed_dict={
-					x: train_x[current:current + batch_size],
-					y_true: train_y[current:current + batch_size],
-					lrt: lr
-				})
-
-				print("Epoch {}/{}. Images from {} to {} of {}. Accuracy: {}".format(epoch, epochs, current,
-																			   current + batch_size, len(train_x), acc))
-				current = current + batch_size
-				mean_train_acc += acc
-				num_batches += 1
-
-			mean_train_acc /= num_batches
-
-			# Test accuracy
-			mean_test_acc = 0
-			current = 0
-			num_batches = 0
-			while current < len(test_x):
-				num_batches += 1
-				test_acc = sess.run(accuracy, feed_dict={
-					x: test_x[current:current + batch_size],
-					y_true: test_y[current:current + batch_size],
-					lrt: lr
-				})
-				mean_test_acc += test_acc
-				current = current + batch_size
-
-			mean_test_acc /= num_batches
-
-			if mean_test_acc > best_test_acc:
-				best_test_acc = mean_test_acc
-
-			tElapsed = time.time() - tStart
-
-			print("End of epoch {}. Train accuracy: {}, Test accuracy: {}, Time: {}s".format(epoch, mean_train_acc,
-																							 mean_test_acc, tElapsed))
-
-			summary = tf.Summary()
-			summary.value.add(tag="Train accuracy", simple_value=mean_train_acc)
-			summary.value.add(tag="Test accuracy", simple_value=mean_test_acc)
-			writer.add_summary(summary, epoch)
-			print("Summary saved!")
-
-			# Save current epoch in file
-			with open(os.path.join(checkpoint_dir, 'current_epoch.txt'), 'w') as f:
-				f.write(str(epoch + 1))
-				f.write('\n')
-				f.write(str(best_test_acc))
-
+	print(results)
+	return results
 
 # def main(db, net_type, batch_size, epochs, checkpoint_dir, log_dir, activation, spp_alpha, lr, momentum):
 
@@ -208,6 +224,7 @@ def experiment(path):
 	lrs = [0.1, 0.01, 0.001]
 	momentums = [0.9, 0.5, 0.1]
 
+
 	for db in dbs:
 		for net_type in net_types:
 			for bs in bss:
@@ -219,11 +236,11 @@ def experiment(path):
 					for spp_alpha in _spp_alphas:
 						for lr in lrs:
 							for momentum in momentums:
-								dirname = "{}/{}_{}_{}_{}_{}_{}_{}".format(path, db, net_type, bs, activation, spp_alpha, lr,
-																		momentum)
-								print("RUNNING {}".format(dirname))
-								train(db, net_type, bs, epochs, dirname, dirname, activation, spp_alpha, lr, momentum)
-								tf.reset_default_graph()
+									dirname = "{}/{}_{}_{}_{}_{}_{}_{}".format(path, db, net_type, bs, activation, spp_alpha, lr,
+																			momentum)
+									print("RUNNING {}".format(dirname))
+									train(db, net_type, bs, epochs, dirname, dirname, activation, spp_alpha, lr, momentum)
+									tf.reset_default_graph()
 
 
 if __name__ == '__main__':
